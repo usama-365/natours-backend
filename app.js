a86338d949defc40cd53b6850a76ca836312b9ef
usama-365 <usama-365@protonmail.com>
2023-02-18 18:13:33 +0500

    Added GET request handling logic for tours resource

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const fs = require("node:fs");
 
 const PORT = 3000;
 
 const app = express();
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello World"
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
     });
 })
 
