595d360cbfac45ba6c88eaa3314dc79c9e3e1aac
usama-365 <usama-365@protonmail.com>
2023-02-22 18:13:55 +0500

    Added environment variables

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
 app.use(express.static(`${__dirname}/public`));
+
+if (process.env.NODE_ENV === 'development')
+    app.use(morgan("dev"));
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+require("dotenv").config({
+    path: "./config.env"
+});
 const app = require("./app");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running locally on port ${PORT}`);
 });
