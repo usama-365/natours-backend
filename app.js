f893b4de492e35fbf7fbf5f3389007c5c9e1dbe2
usama-365 <usama-365@protonmail.com>
2023-02-22 17:41:34 +0500

    Sarted serving static files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
