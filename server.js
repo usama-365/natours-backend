69937a16089b462b6a8c0f83725dd0a6eef676e9
usama-365 <usama-365@protonmail.com>
2023-02-23 16:58:01 +0500

    Connected mongoDB to our app

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-require("dotenv").config({
-    path: "./config.env"
-});
+require("dotenv").config({ path: "./config.env" });
 const app = require("./app");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running locally on port ${PORT}`);
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING.replace("<PASSWORD>", process.env.DB_PASSWORD);
+const EXPRESS_SERVER_PORT = process.env.EXPRESS_PORT || 3000;
+
+mongoose.set('strictQuery', true);
+mongoose.connect(DB_CONNECTION_STRING);
+
+app.listen(EXPRESS_SERVER_PORT, () => {
+    console.log(`Server is running locally on port ${EXPRESS_SERVER_PORT}`);
 });
