886d38b5fde0397c074d54b4f7c3c661fcc908ce
usama-365 <usama-365@protonmail.com>
2023-02-23 22:13:12 +0500

    Created a simple tour model

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const EXPRESS_SERVER_PORT = process.env.EXPRESS_PORT || 3000;
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_CONNECTION_STRING);
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 app.listen(EXPRESS_SERVER_PORT, () => {
     console.log(`Server is running locally on port ${EXPRESS_SERVER_PORT}`);
 });
