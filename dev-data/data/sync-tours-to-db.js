f7f08d73962de096c7187162f61ef2948981e037
usama-365 <usama-365@protonmail.com>
2023-02-24 19:04:20 +0500

    Added a helper script to sync tours.json to db

---
+++ dev-data/data/sync-tours-to-db.js
@@ -0,0 +1,19 @@
+/**
+ * This file is just a helper script that deletes all the data 
+ * from the database and adds the placeholder data from tours.json file
+ */
+require('dotenv').config({
+    path: `${__dirname}/../../config.env`
+});
+const mongoose = require('mongoose');
+const fs = require('node:fs');
+const Tour = require("../../models/tourModel");
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`));
+const DB = process.env.DB_CONNECTION_STRING.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+mongoose.connect(DB, { dbName: process.env.DB_NAME }).then(db => {
+    Tour.deleteMany()
+        .then(() => Tour.create(tours))
+        .then(() => db.connection.close());
+});
