d45eb4d256221918a709b40bcc947bdd5a6bef77
usama-365 <usama-365@protonmail.com>
2023-02-24 17:49:37 +0500

    DELETE request is now working

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -73,9 +73,17 @@ exports.updateTour = async (req, res) => {
     }
 }
 
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: "successful",
-        data: null
-    });
+exports.deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status: "successful",
+            data: null
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: error
+        });
+    }
 }
