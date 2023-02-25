cf5296472fd451cbd56afa257d97a0f76846d139
usama-365 <usama-365@protonmail.com>
2023-02-25 10:07:11 +0500

    Added filtering functionality for GET all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,16 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        // Removing the non-attribute params from the GET query params
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        // Building the query
+        const query = Tour.find(queryObj);
+
+        // Executing the query and sending the response
+        const tours = await query;
         res.status(200).json({
             status: "success",
             data: {
