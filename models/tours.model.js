55a25a52b0ad0947db7f6a2833b5029115ef869f
usama-365 <usama-365@protonmail.com>
2023-07-08 21:16:19 +0500

    Added virtual property for duration in weeks

--- models/tours.model.js
+++ models/tours.model.js
@@ -51,6 +51,13 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date],
+}, {
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+});
+
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7;
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
