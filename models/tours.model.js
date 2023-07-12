5a71af7330b60e8707bb5cc5f4de95010942cd62
usama-365 <usama-365@protonmail.com>
2023-07-12 21:21:37 +0500

    Added aggregation middleware and mongoose validation

--- models/tours.model.js
+++ models/tours.model.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true,
+        minlength: [10, 'A tour name must have more or equal than 10 characters'],
+        maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+        // validate: [validator.isAlpha, 'Tour name can only contain characters']
     },
     slug: String,
     duration: {
@@ -18,11 +23,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
         type: Number,
@@ -32,7 +43,15 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function (val) {
+                return val < this.price;
+            },
+            message: 'Price discount ({VALUE}) should be less than price'
+        }
+    },
     summary: {
         type: String,
         trim: true,
@@ -72,7 +91,15 @@ tourSchema.pre('save', function (next) {
 // Query middlewares, run for every hook that starts with find
 // hide secret tours by default
 tourSchema.pre(/^find/, function (next) {
-    this.find({ secretTour: {$ne: true}});
+    this.find({secretTour: {$ne: true}});
+    next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+    // Add the aggregation criteria to exclude secret tour
+    // at the beginning of the aggregation pipeline
+    this.pipeline().unshift({$match: {secretTour: {$ne: true}}});
     next();
 });
 
