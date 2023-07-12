a2606ce99eb9a9f2cac8810fb9f33b1485510ecd
usama-365 <usama-365@protonmail.com>
2023-07-12 22:04:31 +0500

    Started integrating error handling

--- app.js
+++ app.js
@@ -15,4 +15,26 @@ process.env.NODE_ENV === 'development' && app.use(morgan("dev"));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// Invalid route
+app.all('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl}`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl}`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
+
 module.exports = app;
