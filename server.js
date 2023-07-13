47b2c104fd735cbcbb00fde125d42686ddf697e3
usama-365 <usama-365@protonmail.com>
2023-07-13 15:03:01 +0500

    Improved db connection logic

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({path: "./config.env"});
 const mongoose = require("mongoose");
 
 const app = require("./app");
@@ -6,8 +6,11 @@ const app = require("./app");
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING.replace("<PASSWORD>", process.env.DB_PASSWORD);
 const DB_NAME = process.env.DB_NAME;
 
-mongoose.set('strictQuery', true);
-mongoose.connect(DB_CONNECTION_STRING, { dbName: DB_NAME });
+mongoose.set('strictQuery', true)
+    .connect(DB_CONNECTION_STRING, {dbName: DB_NAME})
+    .then(() => console.log("DB connection successful!"))
+    .catch(console.error);
+
 
 const EXPRESS_SERVER_PORT = process.env.EXPRESS_PORT || 3000;
 
