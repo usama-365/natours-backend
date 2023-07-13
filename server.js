a2ecba42af33ebc85f06934a98036ce2c169648a
usama-365 <usama-365@protonmail.com>
2023-07-13 17:11:32 +0500

    Added unhandled promise rejections handler

--- server.js
+++ server.js
@@ -8,11 +8,19 @@ const DB_NAME = process.env.DB_NAME;
 
 mongoose.set('strictQuery', true)
     .connect(DB_CONNECTION_STRING, {dbName: DB_NAME})
-    .then(() => console.log("DB connection successful!"))
-    .catch(console.error);
+    .then(() => console.log("DB connection successful!"));
 
 const EXPRESS_SERVER_PORT = process.env.EXPRESS_PORT || 3000;
 
-app.listen(EXPRESS_SERVER_PORT, () => {
+const server = app.listen(EXPRESS_SERVER_PORT, () => {
     console.log(`Server is running locally on port ${EXPRESS_SERVER_PORT}`);
+});
+
+// Handling promise rejections
+process.on('unhandledRejection', (err) => {
+    console.error(err.name, err.message);
+    server.close(() => {
+        console.error('UNHANDLED REJECTION. Shutting down');
+        process.exit(1);
+    })
 });
