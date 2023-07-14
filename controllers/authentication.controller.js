fd6f02b5cf9ab54ea1030a62b9c65f1b846c0c14
usama-365 <usama-365@protonmail.com>
2023-07-14 19:48:44 +0500

    Added user signup functionality with JWT

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -1,10 +1,24 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user.model');
 const handleAsyncError = require('../utils/handleAsyncError.util');
 
 exports.signup = handleAsyncError(async (req, res, next) => {
-    const user = await User.create(req.body);
+    const { name, email, password, passwordConfirm } = req.body;
+    const user = await User.create({
+        name,
+        email,
+        password,
+        passwordConfirm
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user
         }
