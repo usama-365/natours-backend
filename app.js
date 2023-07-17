a9def382e750ce6b811152c15bc7192da6fbf91f
usama-365 <usama-365@protonmail.com>
2023-07-17 12:15:03 +0500

    Implemented rate limiting for API route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require("./routers/tours.router");
 const userRouter = require("./routers/users.router");
@@ -8,10 +9,16 @@ const globalErrorHandler = require('./controllers/errors.controller');
 
 const app = express();
 
-// Middlewares
+// Global middlewares
+process.env.NODE_ENV === 'development' && app.use(morgan("dev"));
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-process.env.NODE_ENV === 'development' && app.use(morgan("dev"));
 
 // Mounting routers
 app.use("/api/v1/tours", tourRouter);
