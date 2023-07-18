4727cffe2e1176c3816d060ac284aa1ff451c407
usama-365 <usama-365@protonmail.com>
2023-07-18 21:55:23 +0500

    Added some fake data to DB

--- dev-data/data/sync-tours-to-db.js
+++ dev-data/data/sync-tours-to-db.js
@@ -8,12 +8,20 @@ require('dotenv').config({
 const mongoose = require('mongoose');
 const fs = require('node:fs');
 const Tour = require("../../models/tour.model");
+const User = require("../../models/user.model");
+const Review = require("../../models/review.model");
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`));
 const DB = process.env.DB_CONNECTION_STRING.replace("<PASSWORD>", process.env.DB_PASSWORD);
 
 mongoose.connect(DB, { dbName: process.env.DB_NAME }).then(db => {
     Tour.deleteMany()
         .then(() => Tour.create(tours))
+        .then(() => User.deleteMany())
+        .then(() => User.create(users))
+        .then(() => Review.deleteMany())
+        .then(() => Review.create(reviews))
         .then(() => db.connection.close());
 });
