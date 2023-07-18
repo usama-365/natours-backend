173fa5191fe7b394e01fc5b4e71430ccd2a9e039
usama-365 <usama-365@protonmail.com>
2023-07-18 16:46:49 +0500

    Added virtual populates to add reviews in tour, and remove tour from review

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -4,12 +4,13 @@ const handleAsyncError = require('../utils/handleAsyncError.util');
 exports.getAllReviews = handleAsyncError(async (req, res, next) => {
     const reviews = await Review.find();
 
-    // The populated tour field automatically selects the guides field due to a pre-find hook
-    // Even the select during population only selects the name field
-    // So current workaround is to remove it manually from query result
-    reviews.forEach(review => {
-        if (review.tour) review.tour.guides = undefined;
-    });
+    // // (NOT REQUIRED BECAUSE TOUR POPULATION HAS BEEN TURNED OFF)
+    // // The populated tour field automatically selects the guides field due to a pre-find hook
+    // // Even the select during population only selects the name field
+    // // So current workaround is to remove it manually from query result
+    // reviews.forEach(review => {
+    //     if (review.tour) review.tour.guides = undefined;
+    // });
 
     res.status(200).json({
         status: 'success',
