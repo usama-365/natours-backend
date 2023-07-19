1f747eaef55fa08d6fcd3ae3858e0f5481ad7bd4
usama-365 <usama-365@protonmail.com>
2023-07-19 17:00:02 +0500

    Prevented duplicate reviews and added rounding to average rating

--- models/review.model.js
+++ models/review.model.js
@@ -31,14 +31,9 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true },
 });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
-    // this.populate({
-    //     path: 'user',
-    //     select: 'name photo'
-    // }).populate({
-    //     path: 'tour',
-    //     select: 'name'
-    // });
     this.populate({
         path: 'user',
         select: 'name photo'
