ed30a956fc64b70d710a14601029205f79eab96f
usama-365 <usama-365@protonmail.com>
2023-07-19 22:45:24 +0500

    Built the overview page

--- controllers/views.controller.js
+++ controllers/views.controller.js
@@ -1,8 +1,13 @@
-exports.getOverviewPage = (req, res) => {
+const Tour = require('../models/tour.model');
+const handleAsyncError = require('../utils/handleAsyncError.util');
+
+exports.getOverviewPage = handleAsyncError(async (req, res) => {
+    const tours = await Tour.find();
     res.status(200).render('overview', {
-        title: 'All tours'
+        title: 'All tours',
+        tours
     });
-};
+});
 
 exports.getToursPage = (req, res) => {
     res.status(200).render('tour', {
