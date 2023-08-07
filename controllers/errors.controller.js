30cd3610cd275391a45a8a003f3dde136751becf
usama-365 <usama-365@protonmail.com>
2023-08-07 15:15:28 +0500

    Implemented error page

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,68 +1,97 @@
-const AppError = require('../utils/appError.util');
+const AppError = require("../utils/appError.util");
 
-const sendDevelopmentError = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-        stack: err.stack,
-        error: err
-    });
-}
+const sendDevelopmentError = (err, res, req) => {
+	if (req.originalUrl.startsWith("/api")) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message,
+			stack: err.stack,
+			error: err,
+		});
+	} else {
+		res.status(err.statusCode).render("error", {
+			title: "Something went wrong!",
+			msg: err.message,
+		});
+	}
+};
 
-const sendProductionError = (err, res) => {
-    // Operational errors are expected/trusted errors, so send limited detail
-    if (err.isOperational)
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message
-        });
-    // Unknown errors so generic messages
-    else {
-        // Log error detail and send generic message
-        console.error('ERROR 🔥 ', err);
-        res.status(500).json({
-            status: 'error',
-            message: 'Something went very wrong!'
-        });
-    }
-}
+const sendProductionError = (err, res, req) => {
+	// For API
+	if (req.originalUrl.startsWith("/api")) {
+		// Operational errors are expected/trusted errors, so send limited
+		// detail
+		if (err.isOperational) {
+			res.status(err.statusCode).json({
+				status: err.status,
+				message: err.message,
+			});
+		}// Unknown errors so generic messages
+		else {
+			// Log error detail and send generic message
+			console.error("ERROR 🔥 ", err);
+			res.status(500).json({
+				status: "error",
+				message: "Something went very wrong!",
+			});
+		}
+	} else {
+		// FOR RENDERED WEBSITE
+		if (err.isOperational) {
+			res.status(err.statusCode).render("error", {
+				title: "Something went wront!",
+				msg: err.message,
+			});
+		}// Unknown errors so generic messages
+		else {
+			// Log error detail and send generic message
+			console.error("ERROR 🔥 ", err);
+			res.status(500).json({
+				title: "Something went very wrong!",
+				msg: "Something went very wrong!",
+			});
+		}
+	}
+};
 
 const handleCastErrorDB = err => {
-    const message = `Invalid ${err.path}: ${err.value}.`;
-    return new AppError(400, message);
-}
+	const message = `Invalid ${err.path}: ${err.value}.`;
+	return new AppError(400, message);
+};
 
 const handleDuplicateFieldsDB = err => {
-    const message = `Duplicate field value: ${err.keyValue.name}. Please use another value`;
-    return new AppError(400, message);
-}
+	const message = `Duplicate field value: ${err.keyValue.name}. Please use another value`;
+	return new AppError(400, message);
+};
 
 const handleValidationErrors = err => {
-    const errors = Object.values(err.errors).map(err => err.message);
-    const message = `Invalid input data. ${errors.join('. ')}`;
-    return new AppError(400, message);
-}
+	const errors = Object.values(err.errors).map(err => err.message);
+	const message = `Invalid input data. ${errors.join(". ")}`;
+	return new AppError(400, message);
+};
 
-const handleInvalidSignatureError = err => new AppError(401, 'Invalid token. Please login correctly.');
+const handleInvalidSignatureError = err => new AppError(401, "Invalid token. Please login correctly.");
 
-const handleTokenExpiredError = err => new AppError(401, 'Token has expired. Please login again.');
+const handleTokenExpiredError = err => new AppError(401, "Token has expired. Please login again.");
 
 module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || "error";
 
-    if (process.env.NODE_ENV === 'production') {
-        if (err.name === 'CastError')
-            err = handleCastErrorDB(err);
-        else if (err.code === 11000)
-            err = handleDuplicateFieldsDB(err);
-        else if (err.name === 'ValidationError')
-            err = handleValidationErrors(err);
-        else if (err.name === 'JsonWebTokenError')
-            err = handleInvalidSignatureError(err);
-        else if (err.name === 'TokenExpiredError')
-            err = handleTokenExpiredError(err);
-        sendProductionError(err, res);
-    } else if (process.env.NODE_ENV === 'development')
-        sendDevelopmentError(err, res);
+	if (process.env.NODE_ENV === "production") {
+		if (err.name === "CastError") {
+			err = handleCastErrorDB(err);
+		} else if (err.code === 11000) {
+			err = handleDuplicateFieldsDB(err);
+		} else if (err.name === "ValidationError") {
+			err = handleValidationErrors(err);
+		} else if (err.name === "JsonWebTokenError") {
+			err = handleInvalidSignatureError(err);
+		} else if (err.name === "TokenExpiredError") {
+			err = handleTokenExpiredError(err);
+		}
+		sendProductionError(err, res, req);
+	} else if (process.env.NODE_ENV === "development") {
+		sendDevelopmentError(err, res, req);
+	}
 };
--- controllers/views.controller.js
+++ controllers/views.controller.js
@@ -1,5 +1,6 @@
 const Tour = require("../models/tour.model");
 const handleAsyncError = require("../utils/handleAsyncError.util");
+const AppError = require("../utils/appError.util");
 
 exports.getOverviewPage = handleAsyncError(async (req, res) => {
 	const tours = await Tour.find();
@@ -9,11 +10,15 @@ exports.getOverviewPage = handleAsyncError(async (req, res) => {
 	});
 });
 
-exports.getTourPage = handleAsyncError(async (req, res) => {
+exports.getTourPage = handleAsyncError(async (req, res, next) => {
 	const tour = await Tour.findOne({ slug: req.params.slug }).populate({
 		path: "reviews",
 		fields: "review rating user",
 	});
+
+	if (!tour) {
+		return next(new AppError(404, "There is no tour with that name"));
+	}
 
 	res.status(200).render("tour", {
 		title: tour.name,
