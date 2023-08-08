1e05e701face8ddfa0df814158e328aba30ed6c2
usama-365 <usama-365@protonmail.com>
2023-08-08 23:50:03 +0500

    Vercel related changes

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ process.on("uncaughtException", err => {
 	process.exit(-1);
 });
 
-const app = require("./app");
+const app = require("../app");
 
 const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING.replace("<PASSWORD>", process.env.DB_PASSWORD);
 const DB_NAME = process.env.DB_NAME;
@@ -18,13 +18,13 @@ mongoose.set("strictQuery", true)
 
 const EXPRESS_SERVER_PORT = process.env.EXPRESS_PORT || 3000;
 
-const server = app.listen(EXPRESS_SERVER_PORT, () => {
+const index = app.listen(EXPRESS_SERVER_PORT, () => {
 	console.log(`Server is running locally on port ${EXPRESS_SERVER_PORT}`);
 });
 
 process.on("unhandledRejection", err => {
 	console.error(err.name, err.message);
-	server.close(() => {
+	index.close(() => {
 		console.error("UNHANDLED REJECTION. Shutting down!");
 		process.exit(-1);
 	});
